2e1ba100b2a2e2e0795099df6af088ab82a6373f
Nil Gohil <nilgohil2005@gmail.com>
2026-03-21 16:58:21 +0530

    Fix Google OAuth token scope

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ app.config.update(
 )
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE']  = '1'
 
 db            = SQLAlchemy(app)
 login_manager = LoginManager(app)
